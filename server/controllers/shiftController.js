9c3a3b867541543d89443548ace33c9bcd0e3a9b
faiz hussain <faizhussainpnq@gmail.com>
2026-09-08 15:43:15 +0530

    Fix User model import case sensitivity

--- server/controllers/shiftController.js
+++ server/controllers/shiftController.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+const User = require('../models/user.js');
 const d = () => new Date().toISOString().split('T')[0];
 const t = () => new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
 
